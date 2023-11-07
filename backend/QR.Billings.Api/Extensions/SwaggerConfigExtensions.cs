0363561ba64590f1f0438f356886554a290f4005
Flavio Leal <flasile@gmail.com>
2023-11-07 16:13:49 -0300

    adicionando projeto de api com configuração do swagger

---
+++ backend/QR.Billings.Api/Extensions/SwaggerConfigExtensions.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace QR.Billings.Api.Extensions
+{
+    /// <summary>
+    /// SwaggerConfigExtensions
+    /// </summary>
+    public static class SwaggerConfigExtensions
+    {
+        /// <summary>
+        /// AddSwagger
+        /// </summary>
+        /// <param name="services"></param>
+        public static void AddSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1",
+                    new OpenApiInfo
+                    {
+                        Title = "QR Billing API",
+                        Version = "v1",
+                        Description = ""
+                    });
+
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description =
+                    "Insira o token JWT neste formato: Bearer {token}. \r\n\r\n Example: \"Bearer 12345abcdef\"",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header,
+
+                        },
+                        new List<string>()
+                    }
+                });
+            });
+        }
+    }
+}
