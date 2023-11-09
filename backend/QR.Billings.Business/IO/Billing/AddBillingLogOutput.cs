0677dd3b58edf5286b06fc48e9fce8c8426caaf1
Flavio Leal <flasile@gmail.com>
2023-11-09 20:49:44 -0300

    adicionando billing log

---
+++ backend/QR.Billings.Business/IO/Billing/AddBillingLogOutput.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QR.Billings.Business.IO.Billing
+{
+    public class AddBillingLogOutput
+    {
+        public AddBillingLogOutput(Guid id, decimal value, Guid merchantId, string merchantName)
+        {
+            Id = id;
+            Value = value;
+            MerchantId = merchantId;
+            MerchantName = merchantName;
+        }
+
+        public Guid Id { get; set; }
+        public decimal Value { get; set; }
+        public Guid MerchantId { get; set; }
+        public string MerchantName { get; set; }
+    }
+}
